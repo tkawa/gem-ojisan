e691b3d1c4bf7cc2ed6ad98a7ce69605787cd29c
Shunichi Ikegami <sike.tm@gmail.com>
2016-03-19 02:12:43 +0900

    Add SendGrid settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
+  config.action_mailer.default_url_options = {
+    host: ENV['APP_HOST'],
+    protocol: 'https'
+  }
+  config.action_mailer.asset_host = "https://#{ENV['APP_HOST']}"
 end
