1ad7b31ab4e3a148b9c4a38a877d3bcabbbd0716
Shunichi Ikegami <sike.tm@gmail.com>
2016-03-20 01:12:54 +0900

    Add devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
   validates :name, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'home#index'
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
