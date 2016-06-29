d0fbd8c9ab783a4daa269c31dab0734c2fa6de13
Shunichi Ikegami <sike.tm@gmail.com>
2016-06-29 12:32:06 +0900

    投稿メッセージを修正

--- app/models/gemnasium.rb
+++ app/models/gemnasium.rb
@@ -30,7 +30,7 @@ class Gemnasium
 
   def self.build_message(red_dependencies)
     out = StringIO.new
-    out.puts '![](https://pbs.twimg.com/profile_images/425255790320947201/mNYZcFSq_bigger.jpeg) ヤバい gem が使われてるよー！'
+    out.puts '![](https://pbs.twimg.com/profile_images/425255790320947201/mNYZcFSq_bigger.jpeg) ヤバい gem が使われてるぞー！'
     red_dependencies.each do |project_name, deps|
       out.puts "### #{project_name}"
       out.puts '```'
