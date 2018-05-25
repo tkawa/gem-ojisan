488f3b82e076b8a3d3fed1705dfa2394dad3e800
Toru KAWAMURA <tkawa@4bit.net>
2018-05-25 11:16:54 +0900

    comment out `authenticate_user!`
    (cherry picked from commit a2ea75c)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  http_basic_authenticate_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASS'] # if Rails.env.production?
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASS'] if Rails.env.production?
 
   # before_action :authenticate_user!
   # before_action :configure_permitted_parameters, if: :devise_controller?
